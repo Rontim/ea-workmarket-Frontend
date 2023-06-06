203525a2054a33d747477ec513403b12b2d4a9fa
Edwin574 <owinoedwin574@gmail.com>
2023-06-06 11:01:17 +0300

    starting on job poster info section

--- frontend/src/components/JobPosterInfo.js
+++ frontend/src/components/JobPosterInfo.js
@@ -3,7 +3,9 @@ import "./css/jobs.css"
 const JobPosterInfo = () => {
   return (
     <div className='container my-4'>
-      <h1>Hello Job poster</h1>
+          <div>
+              <h2 className='text-center'>WANT TO POST JOBS ?</h2>
+          </div>
     </div>
   );
 }
